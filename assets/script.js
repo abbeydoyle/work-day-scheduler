7585e2ce7e600814dc3ca523dfc55449d61b6229
Abigail Doyle <abigaildoyle3@gmail.com>
2022-10-31 13:47:33 -0700

    JS render function added

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var $containerEl = $(".container");
 
 var $appointmentEl = $("#appointment")
 
-var appointment = [];
+var appointments = [];
 
 // dddd: full name, Do: day of month, MMMM: full month name, YYYY: full year
 var momentDate = moment().format("dddd, MMMM, Do, YYYY");
@@ -13,8 +13,36 @@ var momentHour = moment().format("H");
 function workdayCalendar() {
       console.log(momentDate);
       console.log(momentHour);
+      console.log(appointment);
 
+      $timeBlock.each(function() {
+            var $thisBlock = $(this);
+            var thisBlockHr = parseInt($thisBlock.attr("block-time"));
+
+            var apptItems = {
+                  hour: thisBlockHr,
+                  text: "",
+            }
+            appointments.push(apptItems);
+      }
+      );
+      localStorage.setItem("appts", JSON.stringify(appointments));  
+}
+
+
+function renderCalendar() {
+      appointments = localStorage.getItem("appts");
+      appointments = JSON.parse("appointments");
+
+      for (var i = 0; i < appointments.length; i++) {
+            var apptHour = appointments[i].hour;
+            var apptText = appointments[i].text;
       
+      $("[block-hour=" + apptHour + "]").children("aside").val(apptText);
+
+      }
+
+      console.log(appointments);
 }
 
 
