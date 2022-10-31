ab0168a63e95e5141d5b7eb570dcaaba2196ea38
Abigail Doyle <abigaildoyle3@gmail.com>
2022-10-31 14:14:46 -0700

    completed JS nonfunctioning

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var $currentDay = $("#currentDay");
 var $containerEl = $(".container");
+var $timeBlocks = $(".time-block");
 
 var $appointmentEl = $("#appointment")
 
@@ -30,6 +31,24 @@ function workdayCalendar() {
 }
 
 
+function initializeTimeBlocks(){
+      $timeBlocks.each(function(){
+        var $thisBlock = $(this);
+        var thisBlockHr = parseInt($thisBlock.attr("block-hour"));
+  
+        if (thisBlockHr == momentHour) {
+          $thisBlock.addClass("present").removeClass("past future");
+        }
+        if (thisBlockHr < momentHour) {
+          $thisBlock.addClass("past").removeClass("present future");
+        }
+        if (thisBlockHr > momentHour) {
+          $thisBlock.addClass("future").removeClass("past present");
+        }
+      });
+  }
+
+
 function renderCalendar() {
       appointments = localStorage.getItem("appts");
       appointments = JSON.parse("appointments");
@@ -46,3 +65,41 @@ function renderCalendar() {
 }
 
 
+function saveCalendar(){
+      var $thisBlock = $(this).parent();
+    
+      var hourToUpdate = $(this).parent().attr("block-hour");
+      var itemToAdd = (($(this).parent()).children("aside")).val();
+    
+
+      for (var j = 0; j < appointments.length; j++){
+        if (appointments[j].hour == hourToUpdate){
+          
+          appointments[j].text = itemToAdd;
+        }
+      }
+
+      localStorage.setItem("appts", JSON.stringify(appointments));
+
+      renderCalendar();
+    }
+
+    $(document).ready(function(){
+
+      //format the timeblocks depending on time
+      initializeTimeBlocks();
+      //if there's nothing for the todos in local storage
+      if(!localStorage.getItem("appts")){
+        //initialize the array of objects
+        initializeCalendar();
+      } //otherwise dont bother bc we get it from local storage
+    
+      //display current date
+      $currentDay.text(momentDate);
+    
+      //render schedule from local storage
+      renderCalendar();
+      //when a todo item save button is clicked, save it
+      $containerEl.on("click", "button", saveCalendar);
+      
+    });
