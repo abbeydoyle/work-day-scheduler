059cf494d49d4d07d24e77af55b5259f053f0e21
Abigail Doyle <abigaildoyle3@gmail.com>
2022-10-30 21:18:42 -0500

    time block formatting

--- assets/script.js
+++ assets/script.js
@@ -7,5 +7,14 @@ var appointment = [];
 
 // dddd: full name, Do: day of month, MMMM: full month name, YYYY: full year
 var momentDate = moment().format("dddd, MMMM, Do, YYYY");
+var momentHour = moment().format("H");
 
-console.log(momentDate);
+
+function workdayCalendar() {
+      console.log(momentDate);
+      console.log(momentHour);
+
+      
+}
+
+
